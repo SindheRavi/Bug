2059facf7b723d3db14c6eef668622ccbff7fd94
EC2 Default User <ec2-user@ip-172-31-36-141.ap-south-1.compute.internal>
2020-12-11 14:37:01 +0000

    Updated changes to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 public static void main(String[] args){
 
-System.out.println("Ravi You are done");
+System.out.println("Ravi You are done. This is another example");
   
 }
 }
